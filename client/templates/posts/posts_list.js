be18efcfefdd44b0f3329684d5d169fc64d127a4
arca223 <arcanis223@gmail.com>
2016-03-16 21:06:55 +0100

    templates & helpers

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Max Le Coq on 16/03/2016.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
